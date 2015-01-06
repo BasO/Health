63bbf5f4bcd9f787fa85a063664f74cb3e36efb5
Bas Oppenheim <Bas@wcw-stud-145-109-7-65.wireless.uva.nl>
2015-01-06 18:35:14 +0100

    Xcode

---
+++ Health15/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Health15
+//
+//  Created by Bas Oppenheim on 06-01-15.
+//  Copyright (c) 2015 Bas Oppenheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
